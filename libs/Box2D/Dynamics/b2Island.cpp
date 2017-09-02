5721b41ec00601f512aa7d90bef51e1d36fba666
Fatdazz <fatdazz@FatDazz.local>
2017-09-02 22:23:16 +0200

    frottement

--- libs/Box2D/Dynamics/b2Island.cpp
+++ libs/Box2D/Dynamics/b2Island.cpp
@@ -208,6 +208,7 @@ void b2Island::Solve(b2Profile* profile, const b2TimeStep& step, const b2Vec2& g
             }else{
 			v += h * (b->m_gravityScale * gravity + b->m_invMass * b->m_force); //alex
             }
+            v -= 0.10*v; // alex modif frotemment 
 			w += h * b->m_invI * b->m_torque;
 
 			// Apply damping.
